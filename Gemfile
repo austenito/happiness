762e2daf972b4631f9fa6309ec157d4203a6ac07
Austen Ito <austen.ito@gmail.com>
2014-04-12 09:32:06 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.4'
+gem 'pg'
+
+group :production do
+  gem 'unicorn'
+end
+
+group :development, :test do
+  gem 'faker'
+  gem 'pry-rails'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+end
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Happiness::Application.config.secret_key_base = '0c7d3227149c5defae7a679e4098ddde12a322214607d1a8d125354b5558bcf4e394b9e4276753ea47d1db9cb820969f1ad3ebf743759198c939adc1adf9779d'
