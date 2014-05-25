c7a1231cca61896eb6eb3aea75e3fb25f94b0143
Austen Ito <austen.ito@gmail.com>
2014-05-25 14:11:34 -0400

    Fix creating survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -9,7 +9,7 @@ class SurveysController < ApplicationController
   end
 
   def create
-    current_user.create_random_survey
+    survey = current_user.create_random_survey
     redirect_to survey_survey_question_path(survey.id, survey.survey_questions.first.id)
   end
 end
