86cd2f1769560ee59683024a69c58bdd9e5f7d34
Austen Ito <austen.ito@gmail.com>
2014-08-25 23:13:36 -0400

    Configure log

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,8 +6,8 @@ working_directory "."
 pid "./pids/unicorn.pid"
 
 # Path to logs
-stderr_path "/log/happiness/unicorn_error.log"
-stdout_path "/log/happiness/unicorn.log"
+stderr_path "/log/happiness_unicorn_error.log"
+stdout_path "/log/happiness_unicorn.log"
 
 # Number of processes
 # worker_processes 4
