0deb23043f7a017eb4b3b3842c18c84970ca8b85
Austen Ito <austen.ito@gmail.com>
2014-08-28 22:08:37 -0400

    Fix unicorn logging

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,8 +6,8 @@ working_directory "."
 pid "./pids/unicorn.pid"
 
 # Path to logs
-stderr_path "/apps/happiness/log/happiness_unicorn_error.log"
-stdout_path "/apps/happiness/log/happiness_unicorn.log"
+stderr_path "/apps/log/happiness_unicorn_error.log"
+stdout_path "/apps/log/happiness_unicorn.log"
 
 # Number of processes
 # worker_processes 4
