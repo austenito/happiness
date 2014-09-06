64dd247a79702c242a83191f2e487593dad3f533
Austen Ito <austen.ito@gmail.com>
2014-09-06 11:17:14 -0400

    Debug

--- config/initializers/poptart.rb
+++ config/initializers/poptart.rb
@@ -2,5 +2,6 @@ if Rails.env.development? || Rails.env.test?
   Poptart.api_token = 'testing'
 else
   Poptart.api_token = ENV['POPTART_API_TOKEN']
+  raise ENV.inspect
   Poptart.url = "http://#{ENV['HAPPINESS_SERVICE_PORT_3000_TCP_ADDR']}:#{ENV['HAPPINESS_SERVICE_PORT_3000_TCP_PORT']}"
 end
