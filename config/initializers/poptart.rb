6f2ec051af06df5eafbf81572794c8f677caa259
Austen Ito <austen.ito@gmail.com>
2014-09-06 11:27:12 -0400

    Rollback

--- config/initializers/poptart.rb
+++ config/initializers/poptart.rb
@@ -2,6 +2,5 @@ if Rails.env.development? || Rails.env.test?
   Poptart.api_token = 'testing'
 else
   Poptart.api_token = ENV['POPTART_API_TOKEN']
-  raise ENV.inspect
   Poptart.url = "http://#{ENV['HAPPINESS_SERVICE_PORT_3000_TCP_ADDR']}:#{ENV['HAPPINESS_SERVICE_PORT_3000_TCP_PORT']}"
 end
