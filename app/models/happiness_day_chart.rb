d6b95b3de5922c053116eb4c0f6842d36ca68919
Austen Ito <austen.ito@gmail.com>
2014-10-12 12:05:47 -0400

    Fix problem of cwday returning 1 indexed days

--- app/models/happiness_day_chart.rb
+++ app/models/happiness_day_chart.rb
@@ -10,6 +10,7 @@ class HappinessDayChart
     survey_questions = @user.service_user.survey_questions_for_key(:how_do_you_feel_right_now)
     survey_questions.each do |survey_question|
       day_of_week = survey_question.created_at.cwday
+      day_of_week = 0 if survey_question.created_at.sunday?
       day_values = @day_to_happiness[day_of_week] << survey_question.answer.to_i
       @day_to_happiness[day_of_week] = day_values
     end
