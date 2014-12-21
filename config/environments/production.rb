deea617bd213a90d41c5058457e7ec8c5dbffcf2
Austen Ito <austen.ito@gmail.com>
2014-12-21 12:38:46 -0500

    Don't minify javascript names so angular knows what to inject

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ Happiness::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.assets.js_compressor = Uglifier.new(mangle: false)
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 end
